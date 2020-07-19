052bfcaa8c9fe82b154525fa744e505debdc6928
Hristo Shabanakov <Shabanakov.Hristo@outlook.com>
2020-07-20 00:32:17 +0200

    Sync

---
+++ build/precache-manifest.3104a2a509eb981676edf78a6c2cf434.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7729ce713c7c1c4a81407f0a7e35cb6f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "776cd0028bdae3f80285",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "ec682ad610efbe4f9538",
+    "url": "/static/js/2.e9bb804e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.e9bb804e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "776cd0028bdae3f80285",
+    "url": "/static/js/main.8ca27186.chunk.js"
+  },
+  {
+    "revision": "e6ce6f78393599f7dd45",
+    "url": "/static/js/runtime-main.37ea6e95.js"
+  }
+]);
---
+++ src/components/header/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Header = () => {
+return (
+    <header>
+        <ul>
+            <li>Home</li>
+            <li>Offers</li>
+            <li>Pharmacies</li>
+        </ul>
+    </header>
+)
+}
+
+export default Header;
