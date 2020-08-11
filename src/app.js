8421377f08873f9993aeb603939ed09dac334a33
Hristo Shabanakov <Shabanakov.Hristo@outlook.com>
2020-08-11 16:15:43 +0200

    Add footer and login page new design.

--- src/app.js
+++ src/app.js
@@ -19,6 +19,7 @@ class App extends Component {
     }
 
     logOut = () => {
+        document.cookie ='Lekarna-token='
         this.setState({
             loggedIn: false,
             user: null
--- src/components/link/index.js
+++ src/components/link/index.js
@@ -1,15 +1,46 @@
-import React from 'react';
+import React, {Component} from 'react';
 import styles from './index.module.css';
 import {Link} from 'react-router-dom';
+import getNavigation from '../../services/navigation';
+import UserContext from '../../Context';
 
-const LinkComponent = ({title, href}) => {
-return (
-    <li className={styles.link}>
-        <Link className={styles.li} to={href}>
-            {title}
-        </Link>
-    </li>
-)
-}
+
+ const LinkComponent = ({title, href}) => {
+ return (
+     <li className={styles.link}>
+         <Link className={styles.li} to={href}>
+             {title}
+         </Link>
+     </li>
+ )
+ }
+
+
+
+
+//  class LinkComponent extends Component  {
+//      static contextType = UserContext;
+
+//      render() {
+//          const {
+//              user
+//          } = this.context;
+
+//          const links = getNavigation(user);
+
+//          return (
+//              links.map(navElement => {
+//                  return (
+//                      <li className={styles.link}>
+//                      <Link className={styles.li} 
+//                      href={navElement.link}>
+//                          {navElement.title}
+//                      </Link>
+//                  </li>
+//                 )
+//              }) 
+//         )
+//      }
+//  }
 
 export default LinkComponent;
