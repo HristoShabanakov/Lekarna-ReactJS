dc48dc1a9c191386cbcd123afe5979baf89f8448
Hristo Shabanakov <Shabanakov.Hristo@outlook.com>
2021-04-12 22:33:35 +0200

    Add Stock page.

---
+++ Front-End/src/pages/allPharmacies/index.js
@@ -0,0 +1,10 @@
+import React, { useState, useContext} from 'react';
+import styles from './index.module.css';
+import Title from '../../components/title';
+import authenticate from '../../services/authenticate';
+import {useHistory} from 'react-router-dom';
+import {Context} from '../../providers/GlobalContextProvider';
+
+const AllPharmaciesPage = () => { 
+
+}
---
+++ Front-End/src/pages/stock/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import styles from './index.module.css';
+import Card from '../../components/card';
+const StockPage = () => {
+    return(
+        <div>
+            <h1 className={styles.h1}>This is Offers Page!</h1>
+            <Card />
+        </div>
+    )
+}
+
+export default StockPage
